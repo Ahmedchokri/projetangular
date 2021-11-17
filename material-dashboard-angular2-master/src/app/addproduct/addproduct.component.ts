77983202d75b1c8d26cb87117f1521dbb09f7c79
ahmedchoko <ahmed.chokri@esprit.tn>
2021-11-17 20:38:43 +0100

    first commit

---
+++ material-dashboard-angular2-master/src/app/addproduct/addproduct.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+import { produit } from 'app/modals/product';
+
+@Component({
+  selector: 'app-addproduct',
+  templateUrl: './addproduct.component.html',
+  styleUrls: ['./addproduct.component.css']
+})
+export class AddproductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ material-dashboard-angular2-master/src/app/listproduct/listproduct.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit,ViewChild,} from '@angular/core';
+
+@Component({
+  selector: 'app-listproduct',
+  templateUrl: './listproduct.component.html',
+  styleUrls: ['./listproduct.component.css']
+})
+export class ListproductComponent implements OnInit {
+
+  constructor() { 
+
+}
+
+  ngOnInit(): void {
+  
+ 
+   
+  }
+
+}
